8b6c9c35ab5c133f6a19447feea71dc48cf13dec
Michael Espinosa <Alexanderbat89@gmail.com>
2024-04-22 18:29:37 -0400

    Agregar archivos de proyecto.

---
+++ Clases/User.cs
@@ -0,0 +1,12 @@
+﻿namespace MiPrimerProyecto.Clases
+{
+    public class User
+    {
+        public int idUsuario { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public string correo { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiPrimerProyecto.Clases;
+
+namespace MiPrimerProyecto.Controllers
+{
+    [Route("user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet(Name = "GetUser")]
+        
+        public User[] Get ()
+        {
+            return [new User {idUsuario = 1, nombres = "alex", apellidos = "rosa", correo = "aRosa@gmail.com", username = "pambe", password = "1234" }];
+        }
+    }
+}
