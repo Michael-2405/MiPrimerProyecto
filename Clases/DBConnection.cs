36f3d13cffdd939db1fed1d209f1a2b76bf5a6fe
Michael Espinosa <Alexanderbat89@gmail.com>
2024-04-25 14:53:51 -0400

    Conexion a la Base de Datos. Manejo de Usuarios.

---
+++ Clases/DBConnection.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace MiPrimerProyecto.Clases
+{
+    public class DBConnection
+    {
+        public static string conn = "Server=DESKTOP-1ML2C0E;Database = TaskDB; Trusted_Connection=True; Integrated Security = True; TrustServerCertificate=True";
+
+    }
+}
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MiPrimerProyecto.Clases;
+using Microsoft.Data.SqlClient;
 
 namespace MiPrimerProyecto.Controllers
 {
@@ -8,11 +9,41 @@ namespace MiPrimerProyecto.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        [HttpGet(Name = "GetUser")]
-        
-        public User[] Get ()
+        [HttpGet]
+        public bool Get (string username, string password)
         {
-            return [new User {idUsuario = 1, nombres = "alex", apellidos = "rosa", correo = "aRosa@gmail.com", username = "pambe", password = "1234" }];
+            bool existe = false;
+            SqlConnection conn = new SqlConnection(DBConnection.conn);
+            conn .Open ();
+            string query = "Select * from Users where Username = @username and Password = @password";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@password", password);
+            SqlDataReader reader = cmd.ExecuteReader();
+            
+            if (reader.Read())
+            {
+                existe = true;
+            }
+            cmd.Dispose();
+            conn.Close ();
+            return existe;
         }
+
+        [HttpPost]
+        public void createUser(string mail, string username, string password)
+        {
+            SqlConnection conn = new SqlConnection (DBConnection.conn);
+            conn .Open ();
+            string consulta = "INSERT INTO Users (Mail, Username, Password) VALUES (@mail, @username, @password)";
+            SqlCommand cmd = new(consulta, conn);
+            cmd.Parameters.AddWithValue("@mail", mail);
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@password", password);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            conn.Close();
+        }
+
     }
 }
