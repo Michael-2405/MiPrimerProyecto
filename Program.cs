9d3a30fca7d1646c69868609a022b930c6710c3e
Adolfo Rosa <adolfoklk0307@gmail.com>
2024-05-09 13:48:01 -0400

    Agregado de estructura HTML.

--- Program.cs
+++ Program.cs
@@ -12,9 +12,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  //  app.UseSwagger();
+  //  app.UseSwaggerUI();
+  app.UseDeveloperExceptionPage();
+
 }
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
